7095c399e95f969b48f3f585fc1233ffe728c9ce
cat <marvinarivera@gmail.com>
2021-10-27 19:40:58 -0400

    practice react #1

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import image from '../App/japanese-dog-shiba-inu-260nw-761358958.jpg';
+
+function App() {
+  return (
+    <div>
+      <h1>Tailwind</h1>
+      <img src={image} alt="dog"></img>
+    </div>
+  );
+}
+
+export default App;
